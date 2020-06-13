719f8fc0b36f45aa69bfbf356f43edd83dc261b9
megidont <qwerty0s.e.m@gmail.com>
2020-06-12 23:47:32 -0500

    added a link to add the bot both in the readme and the file link.txt

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # simon
 Discord bot to dump a list of users ordered by last message time
+
+[Click here to invite him to your server!](https://discord.com/api/oauth2/authorize?client_id=721217077993668700&permissions=68608&scope=bot)
--- bot.js
+++ bot.js
@@ -1,2 +1,11 @@
 var Discord = require('discord.js');
 var auth = require('./auth.json');
+var fs = require('fs');
+
+function writeJSON(json, fname){
+
+	fs.writeFile('./' + fname, JSON.stringify(json), 'utf8', function(err){if(err != null){console.log(err);}});
+
+}
+
+var bot = new Discord.Client();
