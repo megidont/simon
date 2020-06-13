c413f9edf70e63071237186d1869a8bedaa476eb
megidont <qwerty0s.e.m@gmail.com>
2020-06-13 00:56:03 -0500

    Gave up because the API doesn't give you just the last message, it's the last cached message :(

--- bot.js
+++ bot.js
@@ -1,10 +1,11 @@
 var Discord = require('discord.js');
 var auth = require('./auth.json');
 var fs = require('fs');
+var beautify = require('json-beautify');
 
 function writeJSON(json, fname){
 
-	fs.writeFile('./' + fname, JSON.stringify(json), 'utf8', function(err){if(err != null){console.log(err);}});
+	fs.writeFile('./' + fname, beautify(json, null, 2, 110), 'utf8', function(err){if(err != null){console.log(err);}});
 
 }
 
@@ -24,18 +25,36 @@ bot.on('message', async function(message){
 	var at = "<@" + bot.user.id + ">";
 	var nickat = "<@!" + bot.user.id + ">";
 
-	if(message.guild == null){
+	if(message.guild == null && message.author.bot != true){
 
 		message.channel.send("I only work in a server. Please send a message beginning with <@" + bot.user.id + "> in a server for a list of users sorted by last message.");
 
 	}else{
 
-		if(message.content.startsWith(at) || message.content.startsWith(nickat)){
+		if((message.content.startsWith(at) || message.content.startsWith(nickat)) && message.author.bot != true){
 
-			//yaay bot stuff lets make sure I didn't screw the boilerplate real fast
+			message.channel.send("Works so far.");
+			var users = {};
+			for(let m of message.guild.members.cache){
+
+				console.log(m[1].user);
+				if(typeof m[1].user !== 'undefined'){
+					users[m[1].user.username + "#" + m[1].user.discriminator] = {
+
+						id: m[1].user.id,
+						lastMessageSendTime: m[1].lastMessage.createdAt
+
+					}
+				}
+
+			}
+			writeJSON(users, "users.json");
+			message.author.send("And so does this.", {files: [{attachment: './users.json', name: 'users.json'}]}).catch(console.log);
 
 		}
 
 	}
 
 });
+
+bot.login(auth.token);
